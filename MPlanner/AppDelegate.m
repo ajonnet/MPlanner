4831b8a51acf378e5b07091f9eb1f0485e06ae51
Amit <Amit@Amits-MacBook-Pro.local>
2014-08-09 17:59:11 +0530

    MODIFIED: A setting updated for IQKeyboardManager

--- MPlanner/AppDelegate.m
+++ MPlanner/AppDelegate.m
@@ -7,12 +7,16 @@
 //
 
 #import "AppDelegate.h"
+#import <KeyboardManager/KeyboardManager.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    
+    [IQKeyboardManager sharedManager].shouldShowTextFieldPlaceholder = NO;
+    
     return YES;
 }
 							
