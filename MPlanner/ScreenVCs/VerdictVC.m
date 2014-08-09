49fb728e6286e4f8069828f890edb922118ab7f9
Amit <Amit@Amits-MacBook-Pro.local>
2014-08-10 01:52:37 +0530

    ADDED: Implementation done for Verdict VC

--- MPlanner/ScreenVCs/VerdictVC.m
+++ MPlanner/ScreenVCs/VerdictVC.m
@@ -10,40 +10,33 @@
 
 @interface VerdictVC ()
 
+@property (weak, nonatomic) IBOutlet UIView *vResult;
+@property (weak, nonatomic) IBOutlet UIView *vFailure;
+@property (weak, nonatomic) IBOutlet UILabel *lblResult;
 @end
 
 @implementation VerdictVC
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    //Rendering the result
+    Option *option = [self.mMission renderBestOption];
+    self.vResult.hidden = YES;
+    self.vFailure.hidden = YES;
+    if (option) {
+        self.lblResult.text = option.mTitle;
+        self.vResult.hidden = NO;
+    }else {
+        self.vFailure.hidden = NO;
+    }
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - IBAction methods
+- (IBAction)onFinishBtClick:(id)sender {
+    [self.navigationController popToRootViewControllerAnimated:YES];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
